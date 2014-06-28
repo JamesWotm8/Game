1e8fa901035a447940b48ab7f831eb962120c39d
JamesWotm8 <own-ddr@hotmail.com>
2014-06-28 22:00:02 +1200

    Completed Ep1
    create screen, escape to remove screen, Removed cursor from screen,
    problem making it run 60fps.

--- DaGame/DaGame/game.cpp
+++ DaGame/DaGame/game.cpp
@@ -1,24 +1,59 @@
 #include "game.h"
 #include "SDL.h"
+#include <iostream>
+
+namespace{
+	const int sCREENWIDTH = 480;
+	const int sCREENHEIGHT = 640;
+	const int bITSPERPIXEL = 32;
+	const int fPS = 60;
+}
 
 Game::Game(){
 	SDL_Init(SDL_INIT_EVERYTHING); // initiate everything in SDL header
+	SDL_ShowCursor(SDL_DISABLE);
+	screen_ = SDL_SetVideoMode(sCREENHEIGHT, sCREENWIDTH, bITSPERPIXEL, SDL_RESIZABLE);
 	eventLoop(); // the infinite game loop
 }
 
 Game::~Game(){
+	SDL_FreeSurface(screen_); // free the screen 
 	SDL_Quit(); // restore all the resources that SDL was using to the original state
 }
 
 void Game::eventLoop() {
+	SDL_Event event;
+
 	bool running = true;
-	while (running) {
-		// this loop lasts 1/60th of a second which is the same as 1000/60ths ms
-		SDL_Delay(1000/*ms*/ / 60/*fps*/ /*ms*/);
+	while (running) {  // this loop lasts 1/60th of a second which is the same as 1000/60ths ms
+		const int start_time_ms = SDL_GetTicks(); // because it takes time for the whole loop to go through, we need to take into account the time it takes and take it away from the delay.
+		while (SDL_PollEvent(&event)){
+			switch (event.type){
+			case SDL_KEYDOWN:
+				if (event.key.keysym.sym == SDLK_ESCAPE){ // when esc is pressed, stop running 
+					running = false;
+				}
+				break; // break while loop
+			default:
+				break;
+			}
+		}
+
+		update();
+		draw();
+		const int elapsed_time_ms = SDL_GetTicks() - start_time_ms; // end of the loop time - start of the loop time = elapsed time
+		std::cout << "hey" + elapsed_time_ms;
+		SDL_Delay(1000/*ms*/ / fPS /* - elapsed_time_ms commented out because currently not working*/);
 	}
-	// while (running) ~. 60 Hz  ((JUST FINISHED))
-	// handle input. 
-	//
+
+	update();
+	draw();
+}
+
+void Game::update(){
 	// update() . move the player move projectiles check collisions
+}
+
+void Game::draw(){
 	// draw(). draw everything, background and stuff
 }
--- DaGame/DaGame/game.h
+++ DaGame/DaGame/game.h
@@ -1,11 +1,18 @@
 #ifndef GAME_H_ // checks if the token has been defined earlier
 #define GAME_H_	// defines
 
+struct SDL_Surface;
+
 struct Game { // struct is the same as class, but struct's are default to public members while class is private, we want public
 	Game();
 	~Game();  // deconstructor
 private:
 	void eventLoop();
+	void update();
+	void draw();
+
+	SDL_Surface* screen_; // declare the screen
+
 };
 
 #endif // GAME_H_
