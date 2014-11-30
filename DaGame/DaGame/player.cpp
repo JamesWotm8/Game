d2f09c4dcfc9ba615d1c9ec010a7a615f871800e
JamesWotm8 <own-ddr@hotmail.com>
2014-11-30 16:35:15 +1300

    Animated walking/facing left/right
    Quote now walks and stands in the direction that the user inputs the
    keys. Using AnimatedSprite for walking and Sprite for standing

--- DaGame/DaGame/player.cpp
+++ DaGame/DaGame/player.cpp
@@ -13,14 +13,25 @@ namespace{
 	const float kMaxSpeedX = 0.325f; // max speed just pixels per ms
 }
 
+bool operator<(const Player::SpriteState& a, const Player::SpriteState& b){
+	if (a.motion_type != b.motion_type){
+		return a.motion_type < b.motion_type;
+	}
+	if (a.horizontal_facing != b.horizontal_facing){
+		return a.horizontal_facing < b.horizontal_facing;
+	}
+
+	return false;
+}
+
 Player::Player(Graphics& graphics, int x, int y) :
-	x_(x), y_(y), velocity_x_(0.0f), acceleration_x_(0.0f) {
-	sprite_.reset(new AnimatedSprite(graphics, "Images/MyChar.bmp", 0, 0, Game::kTileSize, Game::kTileSize, 15, 3));
+x_(x), y_(y), velocity_x_(0.0f), acceleration_x_(0.0f), horizontal_facing_(LEFT) {
+	initializeSprites(graphics);// change this to the initializeSprite() method which does additional things
 }
 
 
 void Player::update(int elapsed_time_ms){
-	sprite_->update(elapsed_time_ms);
+	sprites_[getSpriteState()]->update(elapsed_time_ms);
 
 	x_ += round(velocity_x_ * elapsed_time_ms); // here is where we accelerate only deals with velocity, no directions
 	velocity_x_ += acceleration_x_ * elapsed_time_ms;
@@ -34,17 +45,32 @@ void Player::update(int elapsed_time_ms){
 }
 
 void Player::draw(Graphics& graphics) {
-	sprite_->draw(graphics, x_, y_);
+	sprites_[getSpriteState()]->draw(graphics, x_, y_);
 }
 
 void Player::startMovingLeft() {
 	acceleration_x_ = -kWalkingAcceleration;
+	horizontal_facing_ = LEFT;
 }
 
 void Player::startMovingRight() {
 	acceleration_x_ = kWalkingAcceleration;
+	horizontal_facing_ = RIGHT;
 }
 
 void Player::stopMoving() {
 	acceleration_x_ = 0.0f;
+}
+
+//the method now has different animations for which direction the player chooses quote to walk/stand
+void Player::initializeSprites(Graphics& graphics){
+	sprites_[SpriteState(WALKING, LEFT)] = boost::shared_ptr<Sprite>(new AnimatedSprite(graphics, "Images/MyChar.bmp", 0, 0, Game::kTileSize, Game::kTileSize, 15, 3)); // take note that both walking sprites for left and right are AnimatedSprites
+	sprites_[SpriteState(STANDING, LEFT)] = boost::shared_ptr<Sprite>(new Sprite(graphics, "Images/MyChar.bmp", 0, 0, Game::kTileSize, Game::kTileSize)); // normal Sprite because of no animation when standing still
+	sprites_[SpriteState(WALKING, RIGHT)] = boost::shared_ptr<Sprite>(new AnimatedSprite(graphics, "Images/MyChar.bmp", 0, Game::kTileSize, Game::kTileSize, Game::kTileSize, 15, 3)); // animated sprite
+	sprites_[SpriteState(STANDING, RIGHT)] = boost::shared_ptr<Sprite>(new Sprite(graphics, "Images/MyChar.bmp", 0, Game::kTileSize, Game::kTileSize, Game::kTileSize)); //sprite
+}
+
+Player::SpriteState Player::getSpriteState(){
+	return SpriteState(acceleration_x_ == 0.0f ? STANDING : WALKING,
+		horizontal_facing_);
 }
--- DaGame/DaGame/player.h
+++ DaGame/DaGame/player.h
@@ -1,8 +1,9 @@
 #ifndef PLAYER_H_
 #define PLAYER_H_
 
-#include <boost/scoped_ptr.hpp>
+#include <boost/shared_ptr.hpp>
 #include "sprite.h"
+#include <map>
 
 struct Graphics;
 
@@ -17,10 +18,40 @@ struct Player {
 	void stopMoving();
 
 private:
+	//types of motion
+	enum MotionType{
+		STANDING,
+		WALKING,
+	};
+
+	//which direction is quote facing
+	enum HorizontalFacing{
+		LEFT,
+		RIGHT,
+	};
+
+
+	//create the map for sprite states
+	struct SpriteState{
+		SpriteState(MotionType motion_type = STANDING, HorizontalFacing horizontal_facing = LEFT) :
+		motion_type(motion_type), horizontal_facing(horizontal_facing) {}
+		MotionType motion_type;
+		HorizontalFacing horizontal_facing;
+	};
+	friend bool operator<(const SpriteState& a, const SpriteState& b);
+
+
 	int x_, y_;
-	boost::scoped_ptr<Sprite> sprite_;
+	
+	std::map<SpriteState, boost::shared_ptr<Sprite>> sprites_;
+
 	float velocity_x_;
 	float acceleration_x_;
+	HorizontalFacing horizontal_facing_;
+
+	void initializeSprites(Graphics& graphics);
+	SpriteState getSpriteState();
+
 };
 
 #endif // PLAYER_H_
